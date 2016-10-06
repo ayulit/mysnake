e367579966983f614439fead1600ff459338c47a
Andrey Litvinov <litwinoff@gmail.com>
2016-10-06 19:51:31 +0300

    Cell one in stack = 1, cell two in stack = 1

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -10,13 +10,22 @@ namespace MySnake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            int x = 1; // cell one in stack = 1
+            func1(x);  // cell two in stack = 1
+            Console.WriteLine("Called func1. x = " + x); // Called func1. x = 1
 
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
+          //  Point p1 = new Point(1,3,'*');
+          //  p1.Draw();
+
+          //  Point p2 = new Point(4,5,'#');
+          //  p2.Draw();
 
             Console.ReadLine();
+        }
+
+        public static void func1(int value)
+        { 
+        
         }
     }
 }
